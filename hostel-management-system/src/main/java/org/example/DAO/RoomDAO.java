94beccaaf01b971e61039f8a570aee9bdb50ba00
Tharshana11 <tharsh.sarath@gmail.com>
2025-01-25 12:13:10 +0530

    Add code RoomDAO

--- hostel-management-system/src/main/java/org/example/DAO/RoomDAO.java
+++ hostel-management-system/src/main/java/org/example/DAO/RoomDAO.java
@@ -34,6 +34,49 @@ public class RoomDAO {
             return "Registering student is unsuccessful! Reason: "+ e.getMessage();
         }
     }
+    public String viewRoom(String room_id, dbConnector conn){
+        String sql = "SELECT * FROM students WHERE room_id = ?;";
+        StringBuilder result = new StringBuilder();
+        try(PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1,id);
+            ResultSet res = preparedStatement.executeQuery();
 
+            if (res.next()){
+                String empId = res.getString("room_id");
+                String name = res.getInt("room_number");
+                int age = res.getInt("capacity");
+                String department = res.getString("available_capacity");
+                return String.format("ID: %s, Name: %s, Age: %d, Department: %s\n", room_id, room_number, capacity, available_capacity);
+
+            } else {
+                return "room records are empty!";
+            }
+        }
+        catch (SQLException e){
+            return "Retrieving room is unsuccessful! Reason: "+ e.getSQLState();
+        }
+    }
+    public String updateRoom(String room_id, String attribute, Object value,Connection conn){
+        String sql = "UPDATE rooms SET " + attribute + " = ? WHERE room_id = ?";
+        try(PreparedStatement preparedStatement = conn.prepareStatement(sql);) {
+
+            if(!attribute.equals("room_number")) {
+                preparedStatement.setString(1, value.toString());
+            }else{
+                preparedStatement.setInt(1,Integer.parseInt(value.toString()));
+            }
+            preparedStatement.setString(2,id);
+
+            int res = preparedStatement.executeUpdate(); // can use  preparedStatement.executeUpdate();
+            if (res>0) {
+                return "Successfully updated the record of the room Id: " + id;
+            } else {
+                return "Updating employee is unsuccessful!";
+            }
+        }
+        catch (SQLException e){
+            return "Updating employee is unsuccessful! Reason: "+ e.getMessage();
+        }
+    }
 
 }
