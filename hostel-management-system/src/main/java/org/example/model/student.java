b05f1b8e656b9c0cd33ba7aa53641684dc9ea3fd
zaina-r <zaienam-se21040@stu.kln.ac.lk>
2025-01-25 09:57:31 +0530

    added student attributes to the student model class

--- hostel-management-system/src/main/java/org/example/model/student.java
+++ hostel-management-system/src/main/java/org/example/model/student.java
@@ -1,4 +1,41 @@
 package org.example.model;
 
 public class student {
+    private static String studentId;
+    private static  String studentName;
+    private static int studentAge;
+    private static String department;
+
+    public static String getStudentId() {
+        return studentId;
+    }
+
+    public static void setStudentId(String studentId) {
+        student.studentId = studentId;
+    }
+
+    public static String getStudentName() {
+        return studentName;
+    }
+
+    public static void setStudentName(String studentName) {
+        student.studentName = studentName;
+    }
+
+    public static int getStudentAge() {
+        return studentAge;
+    }
+
+    public static void setStudentAge(int studentAge) {
+        student.studentAge = studentAge;
+    }
+
+    public static String getDepartment() {
+        return department;
+    }
+
+    public static void setDepartment(String department) {
+        student.department = department;
+    }
+
 }
